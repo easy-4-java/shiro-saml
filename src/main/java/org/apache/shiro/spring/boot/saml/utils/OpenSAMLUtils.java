0eff80a5036b7cf42ed342f6426e6455a8a782c5
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 15:00:11 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/org/apache/shiro/spring/boot/saml/utils/OpenSAMLUtils.java
+++ src/main/java/org/apache/shiro/spring/boot/saml/utils/OpenSAMLUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -25,7 +25,7 @@ import net.shibboleth.utilities.java.support.security.RandomIdentifierGeneration
 /**
  * Utilities for creating OpenSAML objects.
  *
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @SuppressWarnings("unchecked")
 public class OpenSAMLUtils {

6d4c5d6ea7646e66a21ee0b8f86ab234f50c19ab
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 01:47:35 +0800

    refactor: align main with shiro saml 2.0.x

--- shiro-saml-spring/src/main/java/org/apache/shiro/spring/boot/saml/SamlSubjectFactory.java
+++ shiro-saml-spring/src/main/java/org/apache/shiro/spring/boot/saml/SamlSubjectFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -27,7 +27,7 @@ import org.apache.shiro.subject.SubjectContext;
 /**
  * SAML SubjectFactory that preserves remember-me semantics for SAML tokens.
  *
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class SamlSubjectFactory extends SessionCreationEnabledSubjectFactory {
 
